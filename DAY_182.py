0fb4da66af1871957c922d019dd0d998e989206a
Samarth Jain — AI & Python <subhj957@gmail.com>
2026-07-02 22:36:13 +0530

    Added DAY 182

---
+++ DAY_182.py
@@ -0,0 +1,11 @@
+class CoffeeMachine:
+
+    def make_coffee(self):
+        print("Making coffee......")
+
+    def clean_machine(self):
+        print("Cleaning Machine.....")
+
+coffee = CoffeeMachine()
+coffee.make_coffee()
+coffee.clean_machine()
