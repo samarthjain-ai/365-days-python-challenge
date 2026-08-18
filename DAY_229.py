b6b59b720978f9ac7adf3ab05c360c2d98dcbaa5
Samarth Jain — AI & Python <subhj957@gmail.com>
2026-08-18 20:39:12 +0530

    Added DAY 229

---
+++ DAY_229.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a value here: "))
+
+for i in range(1, n+1):
+    for j in range(i):
+        print(i, end="")
+    print()
+
+for i in range(n-1, 0, -1):
+    for j in range(i):
+        print(i, end="")
+    print()
