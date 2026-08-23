8d48cf3c25a79fc7bdf6cd5ec60f13fdd6c1a7e7
Samarth Jain — AI & Python <subhj957@gmail.com>
2026-08-23 13:33:50 +0530

    Added DAY 234

---
+++ DAY_234.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Student(BaseModel):
+    name :str
+    age :int
+    course : str 
+
+class Student_id(BaseModel):
+    name:str
+    age:int
+    course : str
+    id:int
+
+students =[]
+next_id=1
+
+@app.post("/add_student")
+def add_student(student:Student):
+    global next_id
+
+    new_student=Student_id(
+        id=next_id,
+        name=student.name,
+        age =student.age,
+        course=student.course
+        )
+    students.append(new_student)
+    next_id+=1
+
+    return {
+        "Message " : "Succesfuly added the new student ",
+        "Student " : new_student
+        }
+
+@app.get("/show_all_student")
+def show_all_students():
+    return students
+
+@app.get("/student/{id}")
+def student_bt_id(id:int):
+    for student in students:
+        if student.id==id:
+            return student
+    else:
+        return "Student not found"
+
+@app.put("/student/{id}")
+def update_student(id:int,student:Student):
+
+    for old_student in students:
+        if old_student.id ==id:
+            old_student.name=student.name
+            old_student.age=student.age
+            old_student.course=student.course
+
+            return {
+                "message": "Student updated successfully",
+                "student": old_student
+            }
+
+    return {"message": "Student not found"}
+
